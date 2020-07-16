9063d47bf513bfd73e9044339b38993010f90be6
stevejaker <stevenjacobsen@msn.com>
2020-07-16 16:49:01 -0600

    updated dailyScripture.py

--- dailyScripture.py
+++ dailyScripture.py
@@ -8,14 +8,14 @@ from time import sleep, ctime
 import random
 from slackclient import SlackClient
 
-scripture_file = '/home/stevejaker/Programming/Scripts/daily_scripture/scriptures.json'
-master_file = '/home/stevejaker/Programming/Scripts/daily_scripture/master_scriptures.json'
-TOKEN = '' # Insert Private Slack Token Here
+scripture_file = ''
+master_file = ''
+TOKEN = sys.argv[1] # Insert Private Slack Token Here
 sc = SlackClient(TOKEN)
 
 
 
-def sendMessage(text, verse, profile_pic, channel='CN69KLNTY'):
+def sendMessage(text, verse, profile_pic, channel=''):
 	sc.api_call(
 		"chat.postMessage",
 		text     = text,
@@ -59,7 +59,7 @@ def saveList(verse_list):
 
 def getVerse():
 	verse_list = getScriptures()
-	choice = random.randint(0, len(verse_list))
+	choice = random.randint(0, len(verse_list)-1)
 	v = verse_list.pop(choice)
 	# print(verse_list)
 	saveList(verse_list)
@@ -99,22 +99,5 @@ if __name__ == '__main__':
 		reset_from_master()
 	else:
 		main()
-	# HOUR = '12'
-	# MIN = '00'
-	# SEC_RANGE = ['00','01']
-	# main()
-	# sleep(4 * 60 * 60)
-	# main()
-	# while True:
-	# 	# sleep_time = 24 * 60 * 60
-	# 	t = mytime()
-	# 	if t.hour == HOUR and t.min == MIN and t.sec in SEC_RANGE:
-	# 		main()
-	# 		sleep_time = 120
-	# 	else:
-	# 		if '--showtime' in sys.argv:
-	# 			print("{}:{}:{}".format(t.hour,t.min,t.sec))
-	# 		sleep_time = 1
 
-	# 	sleep(sleep_time)
 
